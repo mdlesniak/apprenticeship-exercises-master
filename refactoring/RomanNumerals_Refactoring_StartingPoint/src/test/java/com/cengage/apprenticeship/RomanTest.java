8e6fd6c5e1f11544a59ad2528785744863df0bb8
Michael Lesniak <michael.lesniak@cengage.com>
2024-01-25 20:37:18 -0500

    Cleaning out Eclipse files; udpating Roman Numerals to Java 17 and JUnit 5

--- refactoring/RomanNumerals_Refactoring_StartingPoint/src/test/java/com/cengage/apprenticeship/RomanTest.java
+++ refactoring/RomanNumerals_Refactoring_StartingPoint/src/test/java/com/cengage/apprenticeship/RomanTest.java
@@ -1,35 +1,33 @@
 package com.cengage.apprenticeship;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 /*
  * Instructions:  Please refactor this until it passes the checkstyle rules.  Pay particular
  *                attention to separation of concerns - is there another class (or two, or
  *                three) hiding in there?
  */
 
-public class RomanTest {
+import org.junit.jupiter.api.Test;
 
-	@Test
-	public void testFoo() {
-		Roman roman = new Roman();
-		assertEquals("I", roman.convert(1));
-		assertEquals("II", roman.convert(2));
-		assertEquals("III", roman.convert(3));
-		assertEquals("IV", roman.convert(4));
-		assertEquals("V", roman.convert(5));
-		assertEquals("VI", roman.convert(6));
-		assertEquals("VII", roman.convert(7));
-		assertEquals("VIII", roman.convert(8));
-		assertEquals("IX", roman.convert(9));
-		assertEquals("X", roman.convert(10));
-		assertEquals("XCIX", roman.convert(99));
-		assertEquals("L", roman.convert(50));
-		assertEquals("D", roman.convert(500));
-		assertEquals("M", roman.convert(1000));
+import static org.assertj.core.api.Assertions.assertThat;
 
-	}
+class RomanTest {
 
+    @Test
+    void testFoo() {
+        Roman roman = new Roman();
+        assertThat(roman.convert(1)).isEqualTo("I");
+        assertThat(roman.convert(2)).isEqualTo("II");
+        assertThat(roman.convert(3)).isEqualTo("III");
+        assertThat(roman.convert(4)).isEqualTo("IV");
+        assertThat(roman.convert(5)).isEqualTo("V");
+        assertThat(roman.convert(6)).isEqualTo("VI");
+        assertThat(roman.convert(7)).isEqualTo("VII");
+        assertThat(roman.convert(8)).isEqualTo("VIII");
+        assertThat(roman.convert(9)).isEqualTo("IX");
+        assertThat(roman.convert(10)).isEqualTo("X");
+        assertThat(roman.convert(99)).isEqualTo("XCIX");
+        assertThat(roman.convert(50)).isEqualTo("L");
+        assertThat(roman.convert(500)).isEqualTo("D");
+        assertThat(roman.convert(1000)).isEqualTo("M");
+    }
 }
